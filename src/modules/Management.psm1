8bbe060b0c51d4a63b4bbe5d725fe17e317ae728
Roger Best <rbest@microsoft.com>
2020-07-09 14:05:53 -0700

    Pulling out the download and import code to a seperate module

---
+++ src/modules/Management.psm1
@@ -0,0 +1,205 @@
+<#
+The MIT License (MIT)
+Copyright (c) Microsoft Corporation  
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.  
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+#>
+
+# We are using strict mode for added safety
+Set-StrictMode -Version Latest
+
+# We require a relatively new version of Powershell
+#requires -Version 3.0
+
+# The reason for using the following function and managing errors as done in the cmdlets below is described
+# at the link here: https://github.com/PoshCode/PowerShellPracticeAndStyle/issues/37#issuecomment-347257738
+# The scheme permits writing the cmdlet code assuming the code after an error is not executed,
+# and at the same time allows the caller to decide if the cmdlet *overall* should stop or continue for errors
+# by using the standard ErrorAction syntax. It also mentions the correct cmdlet name in the text for the error
+# without exposing the innards of the function. The price to pay is boilerplate code, reduced by BeginPreamble.
+# You might think you might reduce boilerplate even more by creating a function that takes
+# a scriptBlock and wrap it in the correct begin{} process {try{} catch{}} end {}
+# but that ends up showing the source line of the error as such function, not the cmdlet.
+
+# Import (with . syntax) this at the start of each begin block
+function BeginPreamble {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "", Scope = "Function")]
+    param()
+    Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    $callerEA = $ErrorActionPreference
+    $ErrorActionPreference = 'Stop'
+}
+
+# TODO: consider reducing function below to just get ErrorActionPreference
+# Taken from https://gallery.technet.microsoft.com/scriptcenter/Inherit-Preference-82343b9d
+function Get-CallerPreference {
+    [CmdletBinding(DefaultParameterSetName = 'AllVariables')]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( { $_.GetType().FullName -eq 'System.Management.Automation.PSScriptCmdlet' })]
+        $Cmdlet,
+
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.SessionState]
+        $SessionState,
+
+        [Parameter(ParameterSetName = 'Filtered', ValueFromPipeline = $true)]
+        [string[]]
+        $Name
+    )
+
+    begin {
+        $filterHash = @{ }
+    }
+
+    process {
+        if ($null -ne $Name) {
+            foreach ($string in $Name) {
+                $filterHash[$string] = $true
+            }
+        }
+    }
+
+    end {
+        # List of preference variables taken from the about_Preference_Variables help file in PowerShell version 4.0
+
+        $vars = @{
+            'ErrorView'                     = $null
+            'FormatEnumerationLimit'        = $null
+            'LogCommandHealthEvent'         = $null
+            'LogCommandLifecycleEvent'      = $null
+            'LogEngineHealthEvent'          = $null
+            'LogEngineLifecycleEvent'       = $null
+            'LogProviderHealthEvent'        = $null
+            'LogProviderLifecycleEvent'     = $null
+            'MaximumAliasCount'             = $null
+            'MaximumDriveCount'             = $null
+            'MaximumErrorCount'             = $null
+            'MaximumFunctionCount'          = $null
+            'MaximumHistoryCount'           = $null
+            'MaximumVariableCount'          = $null
+            'OFS'                           = $null
+            'OutputEncoding'                = $null
+            'ProgressPreference'            = $null
+            'PSDefaultParameterValues'      = $null
+            'PSEmailServer'                 = $null
+            'PSModuleAutoLoadingPreference' = $null
+            'PSSessionApplicationName'      = $null
+            'PSSessionConfigurationName'    = $null
+            'PSSessionOption'               = $null
+
+            'ErrorActionPreference'         = 'ErrorAction'
+            'DebugPreference'               = 'Debug'
+            'ConfirmPreference'             = 'Confirm'
+            'WhatIfPreference'              = 'WhatIf'
+            'VerbosePreference'             = 'Verbose'
+            'WarningPreference'             = 'WarningAction'
+        }
+
+
+        foreach ($entry in $vars.GetEnumerator()) {
+            if (([string]::IsNullOrEmpty($entry.Value) -or -not $Cmdlet.MyInvocation.BoundParameters.ContainsKey($entry.Value)) -and
+                ($PSCmdlet.ParameterSetName -eq 'AllVariables' -or $filterHash.ContainsKey($entry.Name))) {
+                $variable = $Cmdlet.SessionState.PSVariable.Get($entry.Key)
+
+                if ($null -ne $variable) {
+                    if ($SessionState -eq $ExecutionContext.SessionState) {
+                        Set-Variable -Scope 1 -Name $variable.Name -Value $variable.Value -Force -Confirm:$false -WhatIf:$false
+                    }
+                    else {
+                        $SessionState.PSVariable.Set($variable.Name, $variable.Value)
+                    }
+                }
+            }
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq 'Filtered') {
+            foreach ($varName in $filterHash.Keys) {
+                if (-not $vars.ContainsKey($varName)) {
+                    $variable = $Cmdlet.SessionState.PSVariable.Get($varName)
+
+                    if ($null -ne $variable) {
+                        if ($SessionState -eq $ExecutionContext.SessionState) {
+                            Set-Variable -Scope 1 -Name $variable.Name -Value $variable.Value -Force -Confirm:$false -WhatIf:$false
+                        }
+                        else {
+                            $SessionState.PSVariable.Set($variable.Name, $variable.Value)
+                        }
+                    }
+                }
+            }
+        }
+
+    } # end
+
+} # function Get-CallerPreference
+
+function Import-RemoteModule {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Web source of the psm1 file")]
+        [ValidateNotNullOrEmpty()]
+        [string] $Source,
+        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Name of the module")]
+        [ValidateNotNullOrEmpty()]
+        [string] $ModuleName,
+        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = "Whether to update and replace an existing psm1 file")]
+        [switch]
+        $Update = $false
+    )
+ 
+    begin { . BeginPreamble }
+    process {
+        try {
+
+            $modulePath = Join-Path -Path (Resolve-Path ./) -ChildPath $ModuleName
+ 
+            $moduleSource = $Source + $ModuleName
+
+            if ($Update -Or !(Test-Path -Path $modulePath)) {
+
+                # Remove-Item -Path $modulePath -ErrorAction SilentlyContinue
+
+                # $WebClient = New-Object System.Net.WebClient
+                # $WebClient.DownloadFile($Source, $modulePath)
+                Download-Module $moduleSource $modulePath
+            }
+   
+            Import-Module $modulePath
+        }
+        catch {
+            Write-Error -ErrorRecord $_ -EA $callerEA
+        }
+    }
+    end {}
+}
+
+function Download-Module {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Web source of the psm1 file")]
+        [ValidateNotNullOrEmpty()]
+        [string] $Source,
+        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Path of the module")]
+        [ValidateNotNullOrEmpty()]
+        [string] $ModulePath
+    )
+  
+    begin { . BeginPreamble }
+    process {
+        try {
+            Remove-Item -Path $ModulePath -ErrorAction SilentlyContinue
+
+            $WebClient = New-Object System.Net.WebClient
+            $WebClient.DownloadFile($Source, $ModulePath)
+        }
+        catch {
+            Write-Error -ErrorRecord $_ -EA $callerEA
+        }
+    }
+    end {}
+}
+
+Export-ModuleMember -Function   Import-RemoteModule,
+                                Download-Module
--- src/modules/Utils.psm1
+++ src/modules/Utils.psm1
@@ -1,3 +1,11 @@
+<#
+The MIT License (MIT)
+Copyright (c) Microsoft Corporation  
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.  
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+#>
+
 # We are using strict mode for added safety
 Set-StrictMode -Version Latest
 
@@ -138,53 +146,6 @@ function Get-CallerPreference {
     } # end
 
 } # function Get-CallerPreference
-
-#function Import-RemoteModule {
-#    param(
-#        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Web source of the psm1 file")]
-#        [ValidateNotNullOrEmpty()]
-#        [string] $Source,
-#        [parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Name of the module")]
-#        [ValidateNotNullOrEmpty()]
-#        [string] $ModuleName,
-#        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = "Whether to update and replace an existing psm1 file")]
-#        [switch]
-#        $Update = $false
-#    )
-#  
-#    $modulePath = Join-Path -Path (Resolve-Path ./) -ChildPath $ModuleName
-#  
-#    if ($Update -Or !(Test-Path -Path $modulePath)) {
-#
-#        Remove-Item -Path $modulePath -ErrorAction SilentlyContinue
-#
-#        $WebClient = New-Object System.Net.WebClient
-#        $WebClient.DownloadFile($Source, $modulePath)
-#    }
-#    
-#    Import-Module $modulePath
-#}
-  
-#function Import-AzLabModule {
-#    [CmdletBinding()]
-#    param(
-#        [parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = "Whether to update an existing Az.LabServices module")]
-#        [switch]
-#        $Update = $false
-#    )
-#
-#    begin { . BeginPreamble }
-#    process {
-#        try {
-#            Import-RemoteModule -Source $AzLabServicesModuleSource -ModuleName $AzLabServicesModuleName -Update:$Update
-#        }
-#        catch {
-#            #Write-Error -ErrorRecord $_ -EA $callerEA
-#            Write-LogFile $_
-#        }
-#    }
-#    end{}
-#}
 
 function Write-LogFile {
     [CmdletBinding()]
@@ -671,7 +632,6 @@ function New-SerializedStringArray {
 }
 
 Export-ModuleMember -Function   Join-DeviceMDM,
-                                Import-AzLabModule,
                                 Write-LogFile,
                                 Write-DebugFile,
                                 Register-ScheduledScriptTask,
